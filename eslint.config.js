5f97fe25efdfc16569e358be96e54ff7dbc50cb9
boris-ramirez <boris97ramirez@gmail.com>
2024-05-16 10:38:34 -0600

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,15 @@
 module.exports = [
   {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+        fetch: "readonly",
+      },
+    },
     rules: {
       "no-unused-vars": "error",
       "no-undef": "error",
