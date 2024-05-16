79ea383be2d4890e6e0aa0cc3f8a1e6e93a31d46
boris-ramirez <boris97ramirez@gmail.com>
2024-05-16 08:36:24 -0600

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,16 @@
+const globals = require("globals");
 module.exports = [
   {
     rules: {
       "no-unused-vars": "error",
       "no-undef": "error",
+    },
+    files: ["**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        myCustomGlobal: "readonly",
+      },
     },
   },
 ];
