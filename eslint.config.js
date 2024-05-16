fa9656aa8abf5440d4338da23b011d3275f1c315
boris-ramirez <boris97ramirez@gmail.com>
2024-05-16 10:13:25 -0600

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,28 @@
 module.exports = [
+  js.configs.recommended, // Usar la configuración recomendada por ESLint
   {
     files: ["**/*.js"],
-    env: {
-      browser: true, // Para que ESLint reconozca las variables globales del navegador como window, document, etc.
-      es2021: true, // O el estándar ECMAScript que estés utilizando
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser, // Importa variables globales del navegador
+        myCustomGlobal: "readonly", // Tu variable global personalizada
+      },
     },
     rules: {
       "no-console": "off", // Ejemplo de regla personalizada
       quotes: ["error", "single"], // Ejemplo de regla para comillas simples
       semi: ["error", "always"], // Ejemplo de regla para punto y coma obligatorio
     },
-    globals: {
-      window: "readonly", // Añadir aquí otras variables globales que necesites
-      document: "readonly",
+    ignores: ["**/temp.js", "config/*"], // Ignorar archivos y directorios específicos
+  },
+  {
+    files: ["tests/**"],
+    languageOptions: {
+      globals: {
+        ...globals.mocha, // Variables globales para Mocha
+      },
     },
   },
 ];
