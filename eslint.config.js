104e58dcc87d2ea9ded689c4b391424851f5a28a
boris-ramirez <boris97ramirez@gmail.com>
2024-05-16 10:08:06 -0600

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,18 @@
-module.exports = [
+export default [
   {
     files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: 2018,
-      globals: {
-        window: "readonly",
-      },
+    env: {
+      browser: true, // Para que ESLint reconozca las variables globales del navegador como window, document, etc.
+      es2021: true, // O el estándar ECMAScript que estés utilizando
     },
     rules: {
-      "no-unused-vars": "error",
-      "no-undef": "error",
+      "no-console": "off", // Ejemplo de regla personalizada
+      quotes: ["error", "single"], // Ejemplo de regla para comillas simples
+      semi: ["error", "always"], // Ejemplo de regla para punto y coma obligatorio
+    },
+    globals: {
+      window: "readonly", // Añadir aquí otras variables globales que necesites
+      document: "readonly",
     },
   },
 ];
