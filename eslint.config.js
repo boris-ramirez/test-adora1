9c47275184ab04208e75e62cf0e827a723cd0a64
boris-ramirez <boris97ramirez@gmail.com>
2024-05-16 09:02:38 -0600

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,17 @@
 const globals = require("globals");
-module.exports = [
-  {
-    rules: {
-      "no-unused-vars": "error",
-      "no-undef": "error",
-    },
-    files: ["**/*.js"],
-    languageOptions: {
-      globals: {
-        window: true,
+
+module.exports = {
+  overrides: [
+    {
+      files: ["**/*.js"],
+      env: {
+        es6: true,
+      },
+      globals: globals.browser,
+      rules: {
+        "no-unused-vars": "error",
+        "no-undef": "error",
       },
     },
-  },
-];
+  ],
+};
