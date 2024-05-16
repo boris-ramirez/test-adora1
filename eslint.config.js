2ec8c3ab572f95f792f00a41ebb8b91f40c8200e
boris-ramirez <boris97ramirez@gmail.com>
2024-05-16 10:29:33 -0600

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,9 @@
-import { configs as jsConfigs } from "@eslint/js";
-import globals from "globals";
-
-export default [
-  jsConfigs.recommended, // Usar la configuración recomendada por ESLint
+const globals = require("globals");
+module.exports = [
   {
-    files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: "module",
-      globals: {
-        ...globals.browser,
-        myCustomGlobal: "readonly",
-      },
-    },
     rules: {
-      "no-console": "off",
-      quotes: ["error", "single"],
-      semi: ["error", "always"],
-    },
-    ignores: ["**/temp.js", "config/*"],
-  },
-  {
-    files: ["tests/**"],
-    languageOptions: {
-      globals: {
-        ...globals.mocha,
-      },
+      "no-unused-vars": "error",
+      "no-undef": "error",
     },
   },
 ];
